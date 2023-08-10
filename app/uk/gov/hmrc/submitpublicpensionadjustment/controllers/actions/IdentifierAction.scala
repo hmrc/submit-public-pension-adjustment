faad1fceadd63c52aecaa09332af13180a84016c
Dan Corbin <125590758+dacohm@users.noreply.github.com>
2023-08-10 08:41:48 +0100

    MCSC-431 Required confidence level set to 250.

--- app/uk/gov/hmrc/submitpublicpensionadjustment/controllers/actions/IdentifierAction.scala
+++ app/uk/gov/hmrc/submitpublicpensionadjustment/controllers/actions/IdentifierAction.scala
@@ -43,7 +43,7 @@ class IdentifierAction @Inject() (val authConnector: AuthConnector, val parser: 
 
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
 
-    authorised(AffinityGroup.Individual and ConfidenceLevel.L50).retrieve(retrievals) {
+    authorised(AffinityGroup.Individual and ConfidenceLevel.L250).retrieve(retrievals) {
       case Some(nino) ~ Some(internalId) ~ Some(affinityGroup) ~ credentialRole =>
         val eventualResult: Future[Result] =
           block(IdentifierRequest(request, nino, internalId, affinityGroup, credentialRole))
