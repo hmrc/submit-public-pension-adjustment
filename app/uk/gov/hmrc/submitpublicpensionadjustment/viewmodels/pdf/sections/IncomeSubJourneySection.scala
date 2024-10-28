5a0673d45e8c93d2a72af3a3c6f732e65a32969d
TJSarno <thomassarno@hotmail.com>
2024-10-28 15:30:00 +0000

    MCSC-981, removed commented out code

--- app/uk/gov/hmrc/submitpublicpensionadjustment/viewmodels/pdf/sections/IncomeSubJourneySection.scala
+++ app/uk/gov/hmrc/submitpublicpensionadjustment/viewmodels/pdf/sections/IncomeSubJourneySection.scala
@@ -28,11 +28,6 @@ import uk.gov.hmrc.submitpublicpensionadjustment.viewmodels.pdf.sections.Declara
 import uk.gov.hmrc.submitpublicpensionadjustment.viewmodels.pdf.{Formatting, Row, Section}
 
 case class IncomeSubJourneySection(
-//  revisedPIA: String,
-//  reducedNetIncome: String,
-//  thresholdIncomeAmount: String,
-//  adjustedIncome: String,
-//  personalAllowance: String,
   incomeSubJourneyTitle: String,
   incomeSubJourneySubSection: Seq[IncomeSubJourneySubSection] = Seq()
 ) extends Section
@@ -40,8 +35,6 @@ case class IncomeSubJourneySection(
 
   override def orderedFieldNames(): Seq[String] =
     Seq("incomeSubJourneyTitle")
-
-  // override def period() = Some(relatingTo)
 
   override def rows(messages: Messages): Seq[Row] = {
 
@@ -122,48 +115,5 @@ object IncomeSubJourneySection extends Formatting {
       case ty: InitialFlexiblyAccessedTaxYear => ty.incomeSubJourney
       case ty: PostFlexiblyAccessedTaxYear    => ty.incomeSubJourney
     }
-
-  //  private def taxYearSchemes(taxYear: TaxYear2016To2023): Seq[TaxYearScheme] =
-//    taxYear match {
-//      case ty: NormalTaxYear                  => ty.taxYearSchemes
-//      case ty: InitialFlexiblyAccessedTaxYear => ty.taxYearSchemes
-//      case ty: PostFlexiblyAccessedTaxYear    => ty.taxYearSchemes
-//    }
-
-  //  def build(finalSubmission: FinalSubmission): Seq[IncomeSubJourneySection] = {
-//    val outOfDates = finalSubmission.calculation
-//      .map(_.outDates)
-//      .getOrElse(Seq.empty)
-//
-//    outOfDates.map(calc => buildFromOutOfDates(calc, finalSubmission))
-//  }
-//
-//  private def buildFromOutOfDates(
-//    calc: OutOfDatesTaxYearsCalculation,
-//    finalSubmission: FinalSubmission
-//  ): IncomeSubJourneySection =
-//    IncomeSubJourneySection(
-//      thresholdIncomeAmount = finalSubmission.submissionInputs.incomeSubJourneyValues.map(_.thresholdIncomeAmount)
-//    )
-//
-//  private def schemePaidChargeSubSections(
-//    finalSubmission: FinalSubmission,
-//    outDateCalc: OutOfDatesTaxYearsCalculation
-//  ): Seq[SchemePaidChargeDetailsSubSection] = {
-//
-//    val outDatesSchemes: Seq[TaxYearScheme] = allTaxYears(finalSubmission)
-//      .filter(ty => ty.period == outDateCalc.period.toCalculationInputsPeriod)
-//      .flatMap(ty => taxYearSchemes(ty))
-//
-//    outDatesSchemes.zipWithIndex.map(schemeWithIndex =>
-//      SchemePaidChargeDetailsSubSection(
-//        schemeWithIndex._2 + 1,
-//        schemeWithIndex._1.chargePaidByScheme,
-//        schemeWithIndex._1.name,
-//        schemeWithIndex._1.pensionSchemeTaxReference
-//      )
-//    )
-//  }
-//
 
 }
