dad3d6f78fd8f1ab961ae14fdebc17ba3a1775e2
Michael Abbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-03-06 16:05:18 +0000

    MCSC-1395 Update libraries (#89)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion = "2.4.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
     "com.beachape"            %% "enumeratum-play-json"         % "1.8.0",
     "org.typelevel"           %% "cats-core"                    % "2.10.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "3.1.0",
     "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "2.1.0",
     "org.apache.xmlgraphics"   % "fop"                          % "2.10",
-    "uk.gov.hmrc"             %% "crypto-json-play-30"          % "8.1.0",
+    "uk.gov.hmrc"             %% "crypto-json-play-30"          % "8.2.0",
     "commons-io"               % "commons-io"                   % "2.14.0"
   )
 
@@ -25,7 +25,9 @@ object AppDependencies {
     "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.17.0"       % Test,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
     "org.mockito"         %% "mockito-scala"           % "1.17.29"        % Test,
-    "org.apache.pdfbox"    % "pdfbox"                  % "2.0.27"         % Test
+    "org.apache.pdfbox"    % "pdfbox"                  % "2.0.27"         % Test,
+    "uk.gov.hmrc"         %% "http-verbs-test-play-30" % "15.2.0"         % Test
+
   )
 
   val itDependencies = Seq(
